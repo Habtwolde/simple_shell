2bada24aada36e2c9a1ed0fa903613151e95ed64
93Helu <zelekeh93@gmail.com>
2022-12-19 10:49:28 -0800

    simple shell

--- aux_help.c
+++ aux_help.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * aux_help_env - Help information for the builtin env
+ * aux_help_env - Help information for the builtin env.
  * Return: no return
  */
 void aux_help_env(void)
