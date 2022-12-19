5b4f4f003229488e59157b8ec86e2b559b1022f8
93Helu <zelekeh93@gmail.com>
2022-12-19 10:59:31 -0800

    simple shell

--- aux_str2.c
+++ aux_str2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strdup - duplicates a str in the heap memory.
+ * _strdup - duplicates a str in the heap memory
  * @s: Type char pointer str
  * Return: duplicated str
  */
